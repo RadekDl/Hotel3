db11292f158f83a4fd33d9642b9ce035f536464b
RadekDl <radek.dl@atlas.cz>
2024-10-15 20:54:17 +0200

    du3
    rezervace změna na bez listu Pokoj , změněný vítisk rezervací, vytvoření extract

--- src/Main.java
+++ src/Main.java
@@ -8,28 +8,21 @@ public class Main {
 
         Host host1 = new Host("Adéla","Malíková", LocalDate.of(1993,3,13));
         Host host2 = new Host("Jan","Dvořáček",LocalDate.of(1995,5,5));
-        List<Host> seznamHostu = new ArrayList<>();
-        seznamHostu.add(host1);
-        seznamHostu.add(host2);
 
-        host2.setDatumNarozeni(LocalDate.of(1995,4,5));
+        List<Host> seznamHostu = listHostu(host1, host2);
+
         System.out.println(host2.getJmeno()+" "+host2.getPrijmeni()+" "+host2.getDatumNarozeni());
 
         Pokoj pokoj1 = new Pokoj(1,1,true,true, BigDecimal.valueOf(1000.0));
         Pokoj pokoj2 = new Pokoj(2,1,true,true, BigDecimal.valueOf(1000.0));
         Pokoj pokoj3 = new Pokoj(3,3,false,true,BigDecimal.valueOf(2400.0));
 
-        List<Pokoj> seznamPokoju = new ArrayList<>();
-        seznamPokoju.add(pokoj1);
-        seznamPokoju.add(pokoj2);
-        seznamPokoju.add(pokoj3);
+        listPokuju(pokoj1, pokoj2, pokoj3);
 
         Rezervace rezervace1 = new Rezervace(pokoj1,1,LocalDate.of(2021,7,19),LocalDate.of(2021,7,26),true,host1.getPrijmeni());
         Rezervace rezervace2 = new Rezervace(pokoj3,2,LocalDate.of(2021,9,1),LocalDate.of(2021,9,14),false,host2.getPrijmeni());
 
-        List<Rezervace> seznamRezervaci =  new ArrayList<>();
-        seznamRezervaci.add(rezervace1);
-        seznamRezervaci.add(rezervace2);
+        List<Rezervace> seznamRezervaci = listRezervaci(rezervace1, rezervace2);
 
         for (Rezervace tiskRezervaci : seznamRezervaci){
             int inedxPocetHostu;
@@ -38,5 +31,27 @@ public class Main {
             +" + "+ inedxPocetHostu+" další host/é.");
 
         }
+    }
+
+    private static List<Host> listHostu(Host host1, Host host2) {
+        List<Host> seznamHostu = new ArrayList<>();
+        seznamHostu.add(host1);
+        seznamHostu.add(host2);
+        host2.setDatumNarozeni(LocalDate.of(1995,4,5));
+        return seznamHostu;
+    }
+
+    private static List<Rezervace> listRezervaci(Rezervace rezervace1, Rezervace rezervace2) {
+        List<Rezervace> seznamRezervaci =  new ArrayList<>();
+        seznamRezervaci.add(rezervace1);
+        seznamRezervaci.add(rezervace2);
+        return seznamRezervaci;
+    }
+
+    private static void listPokuju(Pokoj pokoj1, Pokoj pokoj2, Pokoj pokoj3) {
+        List<Pokoj> seznamPokoju = new ArrayList<>();
+        seznamPokoju.add(pokoj1);
+        seznamPokoju.add(pokoj2);
+        seznamPokoju.add(pokoj3);
     }
 }
